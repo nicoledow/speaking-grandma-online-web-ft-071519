98d93458fa8c7a7139543ef84732d987387144bc
Nicole Dow <nikki.dow71@gmail.com>
2019-07-16 19:32:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-    ""
+    "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == phrase.upcase && phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  else
+    "NO, NOT SINCE 1938!"
   end
 end
